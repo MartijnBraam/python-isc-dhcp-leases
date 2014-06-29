1ad02ae0df2a5643f7af0edbc24d6dbda8a589df
Martijn Braam <martijn@brixit.nl>
2014-06-29 22:21:43 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-isc-dhcp-leases',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/MartijnBraam/python-isc-dhcp-leases',
+    license='MIT',
+    author='Martijn Braam',
+    author_email='martijn@brixit.nl',
+    description='Small python module for reading /var/lib/dhcp/dhcpd.leases from isc-dhcp-server'
+)
