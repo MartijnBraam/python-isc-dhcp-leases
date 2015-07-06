b2979197d6fc35ee25ebb841808b1ec5117765c4
Martijn Braam <martijn@brixit.nl>
2015-07-06 11:32:57 +0200

    Added unit tests

---
+++ isc_dhcp_leases/test_iscDhcpLeases.py
@@ -0,0 +1,47 @@
+from unittest import TestCase
+from isc_dhcp_leases.iscdhcpleases import IscDhcpLeases, Lease
+from freezegun import freeze_time
+from datetime import datetime
+
+__author__ = 'Martijn Braam <martijn@brixit.nl>'
+
+
+class TestIscDhcpLeases(TestCase):
+    @freeze_time("2015-07-6 8:15:0")
+    def test_get(self):
+        leases = IscDhcpLeases("test_files/debian7.leases")
+        result = leases.get()
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].ip, "10.0.0.10")
+        self.assertEqual(result[0].valid, False)
+        self.assertEqual(result[0].hardware, "ethernet")
+        self.assertEqual(result[0].ethernet, "60:a4:4c:b5:6a:dd")
+        self.assertEqual(result[0].hostname, "")
+        self.assertEqual(result[0].start, datetime(2013, 12, 10, 12, 57, 4))
+        self.assertEqual(result[0].end, datetime(2013, 12, 10, 13, 7, 4))
+
+        leases = IscDhcpLeases("test_files/pfsense.leases")
+        result = leases.get()
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].ip, "10.0.10.72")
+        self.assertEqual(result[0].valid, True)
+        self.assertEqual(result[0].hardware, "ethernet")
+        self.assertEqual(result[0].ethernet, "64:5a:04:6a:07:a2")
+        self.assertEqual(result[0].hostname, "Satellite-C700")
+        self.assertEqual(result[0].start, datetime(2015, 7, 6, 7, 50, 42))
+        self.assertEqual(result[0].end, datetime(2015, 7, 6, 8, 20, 42))
+
+    @freeze_time("2015-07-6 8:15:0")
+    def test_get_current(self):
+        leases = IscDhcpLeases("test_files/debian7.leases")
+        result = leases.get_current()
+        self.assertEqual(len(result), 0)
+
+        leases = IscDhcpLeases("test_files/pfsense.leases")
+        result = leases.get_current()
+        self.assertEqual(len(result), 2)
+
+        self.assertTrue("14:da:e9:04:c8:a3" in result)
+        self.assertTrue("64:5a:04:6a:07:a2" in result)
+        self.assertTrue(result["14:da:e9:04:c8:a3"].valid)
+        self.assertTrue(result["64:5a:04:6a:07:a2"].valid)
---
+++ isc_dhcp_leases/test_lease.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+from isc_dhcp_leases.iscdhcpleases import Lease
+from freezegun import freeze_time
+from datetime import datetime
+
+__author__ = 'Martijn Braam <martijn@brixit.nl>'
+
+
+class TestLease(TestCase):
+    def setUp(self):
+        self.lease_data = {
+            'starts': '2 2013/12/10 12:57:04',
+            'uid': '"\\377\\000\\000\\000\\002\\000\\001\\000\\001\\0321\\301\\300\\000#\\213\\360F\\350"',
+            'binding': 'state free',
+            'ends': 'never',
+            'hardware': 'ethernet 60:a4:4c:b5:6a:dd',
+            'cltt': '2 2013/12/10 12:57:04',
+            'tstp': '2 2013/12/10 13:07:04',
+            'client-hostname': '"Satellite-C700"'
+        }
+
+    @freeze_time("2015-07-6 8:15:0")
+    def test_init(self):
+        lease = Lease("192.168.0.1", self.lease_data)
+        self.assertEqual(lease.ip, "192.168.0.1")
+        self.assertEqual(lease.hardware, "ethernet")
+        self.assertEqual(lease.ethernet, "60:a4:4c:b5:6a:dd")
+        self.assertEqual(lease.hostname, "Satellite-C700")
+        self.assertEqual(lease.start, datetime(2013, 12, 10, 12, 57, 4))
+        self.assertIsNone(lease.end)
+        self.assertTrue(lease.valid)
+
+    def test_repr(self):
+        lease = Lease("192.168.0.1", self.lease_data)
+        self.assertEqual(repr(lease), '<Lease 192.168.0.1 for 60:a4:4c:b5:6a:dd (Satellite-C700)>')
+
+    @freeze_time("2015-07-6 8:15:0")
+    def test_valid(self):
+        lease = Lease("192.168.0.1", self.lease_data)
+        self.assertTrue(lease.valid)  # Lease is forever
+
+        lease.end = datetime(2015, 7, 6, 13, 57, 4)
+        self.assertTrue(lease.valid)  # Lease is within start and end
+
+        lease.end = datetime(2015, 7, 6, 6, 57, 4)
+        self.assertFalse(lease.valid)  # Lease is ended
+
+        lease.start = datetime(2015, 7, 6, 12, 57, 4)
+        lease.end = datetime(2015, 7, 6, 13, 57, 4)
+        self.assertFalse(lease.valid)  # Lease is in the future
