951de17eb0bed4a97030738339147565eb7d6be2
andir <andreas@rammhold.de>
2015-08-29 23:15:56 +0200

    Fixed regexp for iaprefix

--- isc_dhcp_leases/iscdhcpleases.py
+++ isc_dhcp_leases/iscdhcpleases.py
@@ -25,7 +25,7 @@ class IscDhcpLeases(object):
         self.regex_leaseblock = re.compile(r"lease (?P<ip>\d+\.\d+\.\d+\.\d+) {(?P<config>[\s\S]+?)\n}")
         self.regex_leaseblock6 = re.compile(r"ia-(?P<type>ta|na|pd) \"(?P<id>[^\"]+)\" {(?P<config>[\s\S]+?)\n}")
         self.regex_properties = re.compile(r"\s+(?P<key>\S+) (?P<value>[\s\S]+?);")
-        self.regex_iaaddr = re.compile(r"iaaddr (?P<ip>[0-9a-f:]+(/[0-9]+)?) {(?P<config>[\s\S]+?)\n\s+}")
+        self.regex_iaaddr = re.compile(r"ia(addr|prefix) (?P<ip>[0-9a-f:]+(/[0-9]+)?) {(?P<config>[\s\S]+?)\n\s+}")
 
     def get(self):
         """
