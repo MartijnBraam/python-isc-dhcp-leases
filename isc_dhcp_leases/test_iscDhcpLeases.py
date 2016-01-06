23db5c5d4b704f4242d1c9c9a9d00358cabffd1d
Martijn Braam <martijn@brixit.nl>
2016-01-06 17:45:43 +0100

    Added testcase for ISC DHCPD 4.3.3 in IPv6 mode

--- isc_dhcp_leases/test_iscDhcpLeases.py
+++ isc_dhcp_leases/test_iscDhcpLeases.py
@@ -35,7 +35,7 @@ class TestIscDhcpLeases(TestCase):
         self.assertEqual(result[0].start, datetime(2015, 7, 6, 7, 50, 42))
         self.assertEqual(result[0].end, datetime(2015, 7, 6, 8, 20, 42))
 
-        leases = IscDhcpLeases("isc_dhcp_leases/test_files/dhcpd6.leases")
+        leases = IscDhcpLeases("isc_dhcp_leases/test_files/dhcpd6-4.2.4.leases")
         result = leases.get()
         self.assertEqual(len(result), 2)
         self.assertEqual(result[0].ip, "2001:610:600:891d::60")
@@ -62,6 +62,34 @@ class TestIscDhcpLeases(TestCase):
         self.assertEqual(result[1].last_communication, datetime(2015, 8, 18, 16, 55, 40))
         self.assertEqual(result[1].type, Lease6.PREFIX_DELEGATION)
 
+        leases = IscDhcpLeases("isc_dhcp_leases/test_files/dhcpd6-4.3.3.leases")
+        result = leases.get()
+        self.assertEqual(len(result), 4)
+        self.assertEqual(result[0].ip, "2001:10:10::106")
+        self.assertEqual(result[0].host_identifier, b"\001\000\000\000\000\001\000\001\034\367\020\245\000'\"3+4")
+        self.assertEqual(result[0].iaid, 1)
+        self.assertEqual(result[0].duid, b"\x00\x01\x00\x01\x1c\xf7\x10\xa5\x00\'\"3+4")
+        self.assertEqual(result[0].valid, True)
+        self.assertEqual(result[0].active, True)
+        self.assertEqual(result[0].binding_state, 'active')
+        self.assertEqual(result[0].preferred_life, 540)
+        self.assertEqual(result[0].max_life, 864)
+        self.assertEqual(result[0].last_communication, datetime(2016, 1, 6, 14, 50, 34))
+        self.assertEqual(result[0].type, Lease6.NON_TEMPORARY)
+
+        self.assertEqual(result[1].ip, "2001:10:30:ff00::/56")
+        self.assertEqual(result[1].host_identifier, b"\x00\x00\x00\x00\x00\x01\x00\x01\x1d4L\x00\x00%\x90k\xa14")
+        self.assertEqual(result[1].iaid, 0)
+        self.assertEqual(result[1].duid, b"\x00\x01\x00\x01\x1d4L\x00\x00%\x90k\xa14")
+        self.assertEqual(result[1].valid, True)
+        self.assertEqual(result[1].active, True)
+        self.assertEqual(result[1].binding_state, 'active')
+        self.assertEqual(result[1].preferred_life, 540)
+        self.assertEqual(result[1].max_life, 864)
+        self.assertEqual(result[1].last_communication, datetime(2016, 1, 6, 14, 52, 37))
+        self.assertEqual(result[1].type, Lease6.PREFIX_DELEGATION)
+
+
     @freeze_time("2015-07-6 8:15:0")
     def test_get_current(self):
         leases = IscDhcpLeases("isc_dhcp_leases/test_files/debian7.leases")
@@ -79,7 +107,7 @@ class TestIscDhcpLeases(TestCase):
 
     def test_get_current_ipv6(self):
         with freeze_time("2015-08-18 17:0:0"):
-            leases = IscDhcpLeases("isc_dhcp_leases/test_files/dhcpd6.leases")
+            leases = IscDhcpLeases("isc_dhcp_leases/test_files/dhcpd6-4.2.4.leases")
             result = leases.get_current()
             self.assertEqual(len(result), 2)
             self.assertIn('na-346476ea000100011d661fe20a0027000000', result)
@@ -89,6 +117,6 @@ class TestIscDhcpLeases(TestCase):
                 self.assertTrue(r.valid, key)
 
         with freeze_time("2015-08-18 18:0:0"):
-            leases = IscDhcpLeases("isc_dhcp_leases/test_files/dhcpd6.leases")
+            leases = IscDhcpLeases("isc_dhcp_leases/test_files/dhcpd6-4.2.4.leases")
             result = leases.get_current()
             self.assertEqual(len(result), 0)
