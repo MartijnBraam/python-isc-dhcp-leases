38c6964fc8e8ee2e0b2f2f61360a533ff8921b9b
Eli Plaut <eli@sentinelone.com>
2016-05-18 14:36:54 +0300

    __init__.py: support for python3 test

--- isc_dhcp_leases/__init__.py
+++ isc_dhcp_leases/__init__.py
@@ -1,2 +1,3 @@
+from __future__ import absolute_import
+from .iscdhcpleases import IscDhcpLeases, Lease, Lease6
 __author__ = 'Martijn Braam <martijn@brixit.nl>'
-from iscdhcpleases import IscDhcpLeases, Lease, Lease6
