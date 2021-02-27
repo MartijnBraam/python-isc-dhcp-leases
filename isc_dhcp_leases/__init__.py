7bd07a3a4a7965bd7a6e0a45de7b30914e1bbced
David Holl <dholl@subspacedynamics.com>
2021-02-27 11:45:52 -0800

    Move string parsing to separate function

--- isc_dhcp_leases/__init__.py
+++ isc_dhcp_leases/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
-from .iscdhcpleases import IscDhcpLeases, Lease, Lease6
+from .iscdhcpleases import IscDhcpLeases, Lease, Lease6, isc_leases_from_string
 __author__ = 'Martijn Braam <martijn@brixit.nl>'
